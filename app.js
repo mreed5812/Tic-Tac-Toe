d348e286c52bca1a31f51a306e37a9d223eef0cc
mreed5812 <mreed5812@gmail.com>
2019-07-22 23:16:45 -0500

    added winning combo arrays/logic, adjusted html/css

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ var playerOneTurn = true;
 var playerTwoTurn = false;
 var playerOneMarker = "X";
 var playerTwoMarker = "O";
+var tableStatus = [];
+var playerStatus = document.getElementById("playerTurn");
+
+var winningCombos = [
+    //winning rows
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    //winning columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    //winning diagonals
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 function markSquare(selectedCell) {
     //mark square with 'X' or 'O'
@@ -21,10 +37,25 @@ function markSquare(selectedCell) {
 function changePlayer() {
     playerOneTurn = !playerOneTurn;
     playerTwoTurn = !playerTwoTurn;
+    if (playerOneTurn) {
+        playerStatus.innerHTML = "Player 1";
+    } else {
+        playerStatus.innerHTML = "Player 2";
+    }
 }            
 
 function winnerDetection() {
+    var x = document.getElementById("board").rows[0].cells.length;
+    var gameTable = document.getElementById("board");
+    tableStatus = [];
 
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            
+            tableStatus.push(document.getElementById("board").rows[i].cells[j].innerHTML);
+            //console.log(document.getElementById("board").rows[i].cells[j].innerHTML);
+        }
+    }
 }
 
 //square.addEventListener('click', squareSelect, false);
@@ -35,14 +66,13 @@ $('td').click(function(e) {
     var row = cell.closest('tr');
     var rowIndex = row[0].rowIndex;
 
-    console.log(cellIndex);
-    console.log(rowIndex);
     var selectedCell = document.getElementById('board').rows[rowIndex].cells[cellIndex];
-    markSquare(selectedCell);
-    var testCellSelection = document.getElementById('board').rows[1].cells[2];
-    testCellSelection.innerHTML = "yes";
-    //winnerDetection();
-    //changePlayer();
+
+    //check if square already marked
+    if (selectedCell.innerHTML == "") {
+        markSquare(selectedCell);
+        changePlayer();
+        winnerDetection();
+    }
 });
 
-const winningCombos = [];
